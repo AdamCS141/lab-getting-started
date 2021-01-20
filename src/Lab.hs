057d400191549ba8cf930ff9b762743dd34aa0c6
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2021-01-20 20:29:21 +0000

    Added backwards goose in lab

--- src/Lab.hs
+++ src/Lab.hs
@@ -13,17 +13,19 @@ import Hatch
 
 animation t =     offset (-400)   200  (dogwheel t)
               <@> offset (-400) (-200) (gooseChase t)
+              <@> offset (-400) (-200) (gooseWalk t)
               <@> offset  400    200   (dogwheel t)
               <@> offset  0      200   (rotate (-t) cat)
               <@> offset (-300)  0     (text "Welcome to CS141!")
 
 -- A load of dogs spinning around
-dogwheel t = superimposeAll [spinningDog x | x <- [t, t + 45 .. t + 315 ] ]
+dogwheel t = superimposeAll [spinningDog x | x <- [t, t + 10 .. t + 350 ] ]
 
 -- One single rotating dog
 spinningDog t = rotate t (offset 100 0 dog)
 
 -- The bottom of the screen goose chase
-gooseChase t = offset (t * 2) 0 (duck <|> duck <|> scale 2 goose)
+gooseChase t = offset (t * 2) 0 (duck <|> duck)
+gooseWalk t = offset (t * (-2)) 0 (scale 2 goose)
 
 --------------------------------------------------------------------------------
